34c613de54a31d4a775a18f56b14375bac1396b6
Rajat S. Lakhina <130336875+rajatslakhina@users.noreply.github.com>
2026-09-10 14:10:14 +0530

    Add DemoApp

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import ContextContract
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContextContractDemoView()
+        }
+    }
+}
